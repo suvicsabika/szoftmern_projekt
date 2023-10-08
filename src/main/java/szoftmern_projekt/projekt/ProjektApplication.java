6b34cf4db9d518e9a137dcf51f15494902c8e3ea
szabi01120 <100777249+szabi01120@users.noreply.github.com>
2023-10-08 15:09:21 +0200

    adding entity + persistence

--- src/main/java/szoftmern_projekt/projekt/ProjektApplication.java
+++ src/main/java/szoftmern_projekt/projekt/ProjektApplication.java
@@ -1,12 +1,7 @@
 package szoftmern_projekt.projekt;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import szoftmern_projekt.projekt.model.Nyilvantartas;
-import szoftmern_projekt.projekt.repository.NyilvantartasRepository;
-
 
 @SpringBootApplication
 public class ProjektApplication {
@@ -24,9 +19,4 @@ public class ProjektApplication {
 	//	public String Szallitmany = "Kokain";
 	//	public String Uticel = "Rotterdam";
 	//	public String Uticel_Ceg = "Rafael és Rómeó KFT.";
-
-	@Bean
-	CommandLineRunner commandLineRunner(NyilvantartasRepository repository) {
-		return args -> repository.save(new Nyilvantartas(null, "Koczka Géza" , "2023-10-07 00:41:20", "2023-10-08 18:30:05", "08:10:10", "00:00:00", false, "Kokain", "Rotterdam", "Rafael és Rómeó KFT."));
-	}
 }
