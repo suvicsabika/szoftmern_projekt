2c8ae52b18d4b9c2e6140f27c7bd204bbaf405a0
szabi01120 <100777249+szabi01120@users.noreply.github.com>
2023-10-08 14:32:29 +0200

    2.felvonas

---
+++ src/main/java/szoftmern_projekt/projekt/repository/NyilvantartasRepository.java
@@ -0,0 +1,7 @@
+package szoftmern_projekt.projekt.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import szoftmern_projekt.projekt.model.Nyilvantartas;
+
+public interface NyilvantartasRepository extends CrudRepository<Nyilvantartas, Integer> {
+}
