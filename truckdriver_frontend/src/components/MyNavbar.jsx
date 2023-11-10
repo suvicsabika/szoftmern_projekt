26b70cad8c66410f74afb1e2952af00d08c0a5ac
szabi01120 <100777249+szabi01120@users.noreply.github.com>
2023-11-10 19:48:36 +0100

    frontend_remake

---
+++ truckdriver_frontend/src/components/MyNavbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import { AcmeLogo } from "./AcmeLogo.jsx";
+
+import "./MyNavbar.css";
+
+export default function MyNavbar() {
+  return (
+    <Navbar shouldHideOnScroll id="navbar">
+      <NavbarBrand>
+        <AcmeLogo />
+        <Link href="../" color="foreground"><p className="text-inherit" id="main-link">Szoft. Mérn. Projekt</p></Link>
+      </NavbarBrand>
+
+      <NavbarContent className=" sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link href="Records" color="foreground" id="nyilvantartas">
+            Nyilvántartás
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="./Home" aria-current="page" id="kezdooldal"> 
+            Kezdőoldal
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="./About" color="foreground" id="leiras">
+            Leírás
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <NavbarItem className=" lg:flex">
+          <Link href="./Login" id="login">Bejelentkezés</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="./Register" variant="flat" id="register">
+            Regisztráció
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
