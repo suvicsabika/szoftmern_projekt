b39efc319ffd92d508a8a66c69c0da983f144ceb
szabi01120 <hajnalszabolcs0@gmail.com>
2023-11-22 12:38:32 +0100

    truckdriver_bootstrap

---
+++ truckdriver_frontend_bts/src/components/MyNavbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function MyNavbar() {
+    return (
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Szoft. Mérn. Projekt</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item top-0">
+                            <a class="nav-link active" aria-current="page" href="#">Kezdőoldal</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <button class="btn btn-primary me-2" type="submit">Bejelentkezés</button>
+                        <button class="btn btn-outline-success" type="submit">Regisztráció</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    )
+}
