e8c5edf19d125772b8547aa15d6064d419ada54b
szabi01120 <100777249+szabi01120@users.noreply.github.com>
2023-11-27 00:21:43 +0100

    email changes, login auth fix, records changes, delete truckdriver_frontend directory - FINAL

--- truckdriver_frontend/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as About } from './About/About';
-export { default as Home } from './Home/Home';
-export { default as Login } from './Login/Login';
-export { default as Records } from './Records/Records';
-export { default as Register } from './Register/Register';
