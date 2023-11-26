ff8c75bcee16ce3ec7f5ef08ff30384778ba5321
suvicsabika <81648169+suvicsabika@users.noreply.github.com>
2023-11-26 21:04:01 +0100

    Final changes

--- src/main/java/szoftmern_projekt/projekt/repository/NyilvantartasRepository.java
+++
@@ -1,7 +0,0 @@
-package szoftmern_projekt.projekt.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import szoftmern_projekt.projekt.model.Nyilvantartas;
-
-public interface NyilvantartasRepository extends CrudRepository<Nyilvantartas, Integer> {
-}
